17328e206be191d590ba1d56b6f8c63bde3bb086
Vince Rose <vrose04@gmail.com>
2022-10-01 14:19:55 -0700

    add last_verison

--- src/index.js
+++ src/index.js
@@ -21,6 +21,13 @@ async function main() {
     core.debug(`prerelease = '${prerelease}'`)
 
     let releaseNotes = ''
+    if (core.getInput('last_version')) {
+        core.debug(`last-version = '${core.getInput('last_version')}'`)
+        let lastVersion = core.getInput('last_version')
+        // remove v prefix
+        earliestVersion = earliestVersion.replace(/^v/, '')
+        releaseNotes = await extractReleaseNotesMultiple(changelogFile, null, lastVersion)
+
     if (core.getInput('earliest_version')) {
         core.debug('earliest-version = true')
         let earliestVersion = core.getInput('earliest_version')
@@ -37,7 +44,7 @@ async function main() {
     core.setOutput("release_notes", releaseNotes)
 }
 
-async function extractReleaseNotesMultiple(changelogFile, earliestVersion) {
+async function extractReleaseNotesMultiple(changelogFile, earliestVersion, lastVersion) {
     const fileStream = fs.createReadStream(changelogFile, {encoding: encoding})
     const rl = readline.createInterface({
         input: fileStream
@@ -47,8 +54,8 @@ async function extractReleaseNotesMultiple(changelogFile, earliestVersion) {
     let earliest_release_found = false
     let start_of_releases = false
     for await (const line of rl) {
+        const check_for_release_block = !!line.match("^#+ \\[[0-9]")
         if (!start_of_releases) {
-            const check_for_release_block = !!line.match("^#+ \\[[0-9]")
             if (!check_for_release_block) {
                 core.debug(`skip line: '${line}'`)
                 continue
@@ -57,18 +64,28 @@ async function extractReleaseNotesMultiple(changelogFile, earliestVersion) {
             }
         }
 
-        const check_for_release_block = !!line.match("^#+ \\[[0-9]")
-        const earliest_release_block = !!line.match(`^#+ \\[(${earliestVersion})]`)
-        if (earliest_release_block) {
-            earliest_release_found = true
-            core.debug(`earliest release found. exiting after this block: '${line}'`)
-            lines.push(line)
-        } else if (check_for_release_block && earliest_release_found) {
-            core.debug(`next release found: '${line}'`)
-            break
-        } else {
-            lines.push(line)
+        if (earliestVersion) {
+            const earliest_release_block = !!line.match(`^#+ \\[(${earliestVersion})]`)
+            if (earliest_release_block) {
+                earliest_release_found = true
+                core.debug(`earliest release found. exiting after this block: '${line}'`)
+                lines.push(line)
+            } else if (check_for_release_block && earliest_release_found) {
+                core.debug(`next release found: '${line}'`)
+                break
+            } else {
+                lines.push(line)
+            }
+        } else if (lastVersion) {
+            const last_release_block = !!line.match(`^#+ \\[(${lastVersion})]`)
+            if (last_release_block) {
+                core.debug(`last release found. exiting: '${line}'`)
+                break
+            } else {
+                lines.push(line)
+            }
         }
+
     }
 
     let releaseNotes = lines.reduce((previousValue, currentValue) => previousValue + eol + currentValue)
